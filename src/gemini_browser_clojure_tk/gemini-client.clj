990a11cd9fab23cc4ab73305160872a8361a8606
Tomasz Kawalec <tkawalec369@gmail.com>
2025-11-29 00:01:37 +0100

    Fix printing response - WIP

--- src/gemini_browser_clojure_tk/gemini-client.clj
+++ src/gemini_browser_clojure_tk/gemini-client.clj
@@ -17,11 +17,15 @@
       (checkClientTrusted [chain auth-type]
         (println (format "Certificate chain:\n%s" (reduce
           (fn [a b] (format "%s\n%s" a b))
-          (map (fn [cert] (format "principal: %s, %s" (.getSubjectX500Principal cert) (if (.checkValidity cert) "valid" "invalid"))) chain)))))
+          (map (fn [cert] (format "principal: %s, %s"
+            (.getSubjectX500Principal cert)
+            (if (.checkValidity cert) "valid" "invalid"))) chain)))))
       (checkServerTrusted [chain auth-type]
         (println (format "Certificate chain:\n%s" (reduce
           (fn [a b] (format "%s\n%s" a b))
-          (map (fn [cert] (format "principal: %s, %s" (.getSubjectX500Principal cert) (if (.checkValidity cert) "valid" "invalid"))) chain)))))
+          (map (fn [cert] (format "principal: %s, %s"
+            (.getSubjectX500Principal cert)
+            (if (.checkValidity cert) "valid" "invalid"))) chain)))))
       (getAcceptedIssuers [] (into-array java.security.cert.X509Certificate [])))
     ssl-ctx (javax.net.ssl.SSLContext/getInstance "TLS")
     _ (.init ssl-ctx nil (into-array javax.net.ssl.TrustManager [trust-mgr]) nil)
@@ -35,7 +39,9 @@
     in-data (.readAllBytes in-stream)
 
     [resp-header resp-body] (split-with (partial not= \return) in-data)]
-  (println (format "Got response: %s\n%s" (doall resp-header) (doall resp-body)))))
+  (println (format "Got response: %s\n%s"
+    (->> resp-header (map char) (apply str))
+    (->> resp-body (map char) (apply str))))))
 
 (defn tab-state-client-watch []
   (fn [watch-key _ _ state-new]))

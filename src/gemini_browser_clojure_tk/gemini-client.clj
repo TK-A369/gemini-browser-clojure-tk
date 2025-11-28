fa4b6f394beda712b1298069586b1df3c7e09066
Tomasz Kawalec <tkawalec369@gmail.com>
2025-11-28 22:57:45 +0100

    Implement custom SSL/TLS TrustManger

--- src/gemini_browser_clojure_tk/gemini-client.clj
+++ src/gemini_browser_clojure_tk/gemini-client.clj
@@ -10,18 +10,32 @@
         1965
         port))
     _ (println (format "Requesting %s (host %s at port %d))" url url-host url-port))
-    socket-factory (javax.net.ssl.SSLSocketFactory/getDefault)
+
+    ; key-mgr-factory (javax.net.ssl.KeyManagerFactory/getInstance (.getDefaultAlgorithm javax.net.ssl.KeyManagerFactory/getInstance))
+    ; _ (.init key-mgr-factory key-store key-store-password
+    trust-mgr (proxy [javax.net.ssl.TrustManager javax.net.ssl.X509TrustManager] []
+      (checkClientTrusted [chain auth-type]
+        (println (format "Certificate chain:\n%s" (reduce
+          (fn [a b] (format "%s\n%s" a b))
+          (map (fn [cert] (format "principal: %s, %s" (.getSubjectX500Principal cert) (if (.checkValidity cert) "valid" "invalid"))) chain)))))
+      (checkServerTrusted [chain auth-type]
+        (println (format "Certificate chain:\n%s" (reduce
+          (fn [a b] (format "%s\n%s" a b))
+          (map (fn [cert] (format "principal: %s, %s" (.getSubjectX500Principal cert) (if (.checkValidity cert) "valid" "invalid"))) chain)))))
+      (getAcceptedIssuers [] (into-array java.security.cert.X509Certificate [])))
+    ssl-ctx (javax.net.ssl.SSLContext/getInstance "TLS")
+    _ (.init ssl-ctx nil (into-array javax.net.ssl.TrustManager [trust-mgr]) nil)
+
+    socket-factory (.getSocketFactory ssl-ctx)
     socket (.createSocket socket-factory url-host url-port)
     _ (.startHandshake socket)
+
     out-stream (.getOutputStream socket)
     in-stream (.getInputStream socket)
-    in-buf-stream (java.util.stream.Stream/new in-stream)
-    _ (.print out-stream (format "%s\r\n" url-host))
-    _ (.flush out-stream)
-    lines (.lines in-buf-stream)
-    resp-header (first lines)
-    resp-body (rest lines)]
-  (println (format "Got response: %s\n%s" resp-header resp-body))))
+    in-data (.readAllBytes in-stream)
+
+    [resp-header resp-body] (split-with (partial not= \return) in-data)]
+  (println (format "Got response: %s\n%s" (doall resp-header) (doall resp-body)))))
 
 (defn tab-state-client-watch []
   (fn [watch-key _ _ state-new]))
